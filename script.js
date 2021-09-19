823cd13b28b78748cd58e49f5458f54bda8340f2
caadams4 <charlesaadams4@outlook.com>
2021-09-19 11:09:25 -0400

    y

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
 import * as rtdb from"https://www.gstatic.com/firebasejs/9.0.2/firebase-database.js";
 // TODO: Add SDKs for Firebase products that you want to use
-
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -18,55 +17,44 @@ const firebaseConfig = {
   measurementId: "G-KVM4FVRTY8"
 };
 
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 let db = rtdb.getDatabase(app);
 let titleRef = rtdb.ref(db, "/");
 let chatRef = rtdb.child(titleRef,"chat");
-let userRef = rtdb.child(titleRef,"user");
-
-document.querySelector("#dbInput").addEventListener('keyup',function(e) {
-    if (e.key === 'Enter') {
-      renderChat();
-}});
-document.querySelector("#inputBtn").addEventListener('click',function(e) {
-        renderChat();
-});
-  
-
-let renderChat = function() {
-pushChat();
-}
 
 let pushChat = function() {
   let message = document.querySelector("#dbInput").value;
-  let newMessage = {"message": message}
+  let alias = document.querySelector("#dbAlias").value;
+  let newMessage = {"message": message, "sender": alias}
   rtdb.push(chatRef,newMessage);
-  updateChat();
+  document.querySelector("#dbInput").innerText= "";
 }
 
-let updateChat = function() {
-document.querySelector("#chatBox").innerText= "";
+
 rtdb.onValue(chatRef, ss=>{
-  let data = ss.val();
-  ss.forEach(function(item){
+document.querySelector("#chatBox").innerText= "";
+let data = ss.val();
+ss.forEach(function(item){
     console.log("made it")
     let chat = JSON.stringify(item.val().message);
+    let sender = JSON.stringify(item.val().sender);
     console.log(chat);
-    $("#chatBox").append('<ul>' + chat +'</ul>');
+    $("#chatBox").append('<ul>' + sender + ": " + chat +'</ul>' + '<br>');
   })
 })
-}
-             
-updateChat();
-
-//      let msg = data[akey];
-//      alert("ln2")
-//      alert(msg);
-  
 
 
+
+document.querySelector("#dbInput").addEventListener('keyup',function(e) {
+    if (e.key === 'Enter') {
+      pushChat();
+          document.querySelector("#dbInput").innerText= "";
+}});
+document.querySelector("#inputBtn").addEventListener('click',function(e) {
+        pushChat();
+    document.querySelector("#dbInput").innerText= "";
+});
 
 //Object.keys(thedata).map(akey=>{ let msg=thedata[akey]})
   
